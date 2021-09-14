d45317c00dadccda7070413b0621b4180b9c3738
alexey.lazarenko <dzotic9@gmail.com>
2021-09-15 00:12:23 +0300

    JE-59864 [ BitNinja Add-on ] Add BitNinja logs into 'Logs' button in Jelastic templates

--- scripts/enableLogsVisibility.sh
+++ scripts/enableLogsVisibility.sh
@@ -4,12 +4,13 @@ nodeType=$1;
 pathLog="/var/log/";
 
 case "$1" in
-"apache") pathLog="/var/log/httpd";
-"maven") pathLog="/var/log/maven/";
-"memcached") pathLog="/var/log/memcached";
-"nginx") pathLog="/var/log/nginx";
-"nginxphp") pathLog="/var/log/nginx";
-"varnish") pathLog="/var/log/varnish";
+"apache" ) pathLog="/var/log/httpd";;
+"maven" ) pathLog="/var/log/maven/";;
+"memcached" ) pathLog="/var/log/memcached";;
+"nginx" ) pathLog="/var/log/nginx";;
+"nginxphp" ) pathLog="/var/log/nginx";;
+"varnish" ) pathLog="/var/log/varnish";;
+esac
 
 ln -s /var/log/bitninja ${pathLog}
 ln -s /var/log/bitninja-ssl-termination ${pathLog}
