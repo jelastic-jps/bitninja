748936b33dbbb9d1c0d0de3f33b305f727941feb
alexey.lazarenko <dzotic9@gmail.com>
2022-01-06 11:57:53 +0200

    JE-59864 [ BitNinja Add-on ] Add BitNinja logs into 'Logs' button in Jelastic templates

--- scripts/enableLogsVisibility.sh
+++ scripts/enableLogsVisibility.sh
@@ -3,17 +3,18 @@
 nodeType=$1;
 pathLog="";
 
-case "$1" in
+case "$nodeType" in
 "apache" ) pathLog="/var/log/httpd";;
 "maven" ) pathLog="/var/log/maven/";;
 "memcached" ) pathLog="/var/log/memcached";;
 "nginx" ) pathLog="/var/log/nginx";;
 "nginxphp" ) pathLog="/var/log/nginx";;
 "varnish" ) pathLog="/var/log/varnish";;
+"tomcat" ) pathLog="/opt/tomcat/logs";;
 esac
 
-[[ ! -z ${pathLog} ]] && {
-  [ ! -d "/var/log/bitninja" ] && ln -s /var/log/bitninja ${pathLog}
-  [ ! -d "/var/log/bitninja-ssl-termination" ] && ln -s /var/log/bitninja-ssl-termination ${pathLog}
-}
+[[ -z ${pathLog} ]] && pathLog="/var/log"
+
+[[ -d "/var/log/bitninja" && ! -d "${pathLog}/bitninja" ]] && ln -s /var/log/bitninja ${pathLog}
+[[ -d "/var/log/bitninja-ssl-termination" && ! -d "${pathLog}/bitninja-ssl-termination" ]] && ln -s /var/log/bitninja-ssl-termination ${pathLog}
 exit 0;
