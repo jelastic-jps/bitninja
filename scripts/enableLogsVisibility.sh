4af8601102b7ec7ceba10cc5561f59f3aab2e78b
alexey.lazarenko <dzotic9@gmail.com>
2022-01-12 17:29:27 +0200

    JE-61296 [ BitNinja Add-on ] BitNinja "logs" are not displayed in some templates node logs

--- scripts/enableLogsVisibility.sh
+++ scripts/enableLogsVisibility.sh
@@ -1,26 +1,6 @@
 #/bin/bash
 
-nodeType=$1;
-pathLog="";
-
-case "$nodeType" in
-nginx* ) pathLog="/var/log/nginx";;
-varnish* ) pathLog="/var/log/varnish";;
-apache* ) pathLog="/var/log/httpd";;
-tomcat* ) pathLog="/opt/tomcat/logs";;
-tomee* ) pathLog="/opt/tomcat/logs";;
-jetty* ) pathLog="/opt/jetty/logs";;
-glassfish3* ) pathLog="/opt/glassfish3/glassfish/domains/domain1/logs";;
-nodejs* ) pathLog="/opt/nodejs/logs";;
-springboot* ) pathLog="/var/log/springboot";;
-mysql* ) pathLog="/var/log/mysql";;
-mariadb* ) pathLog="/var/log/mysql";;
-"postgresql" ) pathLog="/var/log/";;
-postgres* ) pathLog="/var/lib/pgsql/data/pg_log";;
-"memcached" ) pathLog="/var/log/memcached";;
-"maven" ) pathLog="/var/log/maven/";;
-esac
-
+pathLog=$1;
 [[ -z ${pathLog} ]] && pathLog="/var/log"
 
 [[ -d "/var/log/bitninja" && ! -d "${pathLog}/bitninja" ]] && ln -s /var/log/bitninja ${pathLog}
