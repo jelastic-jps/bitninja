ffa124cfcafdc179f5d4e062448aa60c0546a284
alexey.lazarenko <dzotic9@gmail.com>
2022-01-13 15:35:05 +0200

    Delete enableLogsVisibility.sh

--- scripts/enableLogsVisibility.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-
-pathLog=$1;
-[[ -z ${pathLog} ]] && pathLog="/var/log"
-
-[[ -d "/var/log/bitninja" && ! -d "${pathLog}/bitninja" ]] && ln -s /var/log/bitninja ${pathLog}
-[[ -d "/var/log/bitninja-ssl-termination" && ! -d "${pathLog}/bitninja-ssl-termination" ]] && ln -s /var/log/bitninja-ssl-termination ${pathLog}
-exit 0;
